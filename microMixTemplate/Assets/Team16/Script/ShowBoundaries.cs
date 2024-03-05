6f62dbf96003e942deb7dea46a71578ea7a1079a
Aditya Vashishtha <avtmax007@gmail.com>
2024-03-05 13:39:08 -0500

    Showboundary

---
+++ microMixTemplate/Assets/Team16/Script/ShowBoundaries.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ShowBoundaries : MonoBehaviour
+{
+    void OnDrawGizmosSelected()
+    {
+        // Draw boundaries using Gizmos
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, transform.localScale);
+    }
+
+    void Update()
+    {
+        // Print transform values using Debug.Log
+        Debug.Log("Position: " + transform.position);
+        Debug.Log("Rotation: " + transform.rotation.eulerAngles);
+        Debug.Log("Scale: " + transform.localScale);
+    }
+}
